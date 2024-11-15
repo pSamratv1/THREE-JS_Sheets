3dd4b47e3103ad0ea074350163af6a14289d00cd
CODE-SAMRAT <paharisamrat@gmail.com>
2024-11-15 21:52:31 +0545

    Texture added

--- three-js-learning/src/texture.js
+++ three-js-learning/src/texture.js
@@ -1,14 +1,18 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
+const textureLoader = new THREE.TextureLoader();
+const textureTest = textureLoader.load(
+  "/texture/whispy-grass-meadow-bl/wispy-grass-meadow_albedo.png"
+);
 
 const material = new THREE.MeshPhysicalMaterial({
-  clearcoat: 0.5,
-  clearcoatRoughness: 0.2,
-  reflectivity: 0.7,
-  sheen: 0.3,
-  color: 0x2194ce,
+  clearcoat: 0.7,
+  clearcoatRoughness: 0.9,
+  reflectivity: 0.2,
 });
+material.map = textureTest;
 
 const geometry = new THREE.SphereGeometry(7, 64, 32);
 const object = new THREE.Mesh(geometry, material);
@@ -32,11 +36,18 @@ scene.add(pointLight);
 
 const canvas = document.querySelector("#threejs-canvas");
 
+const controls = new OrbitControls(camera, canvas);
+controls.autoRotate = true;
+controls.enableDamping = true;
+controls.enableZoom = false;
+controls.dampingFactor = 0.05;
+
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 
 function animate() {
+  controls.update();
   window.requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
